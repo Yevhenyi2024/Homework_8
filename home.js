798d5e7aac38021d1ba0ab07873477b551277711
Melnyk Yevhenii <karimovzena457@gmail.com>
2024-07-16 18:50:01 +0300

    homework completed

---
+++ home.js
@@ -0,0 +1,7 @@
+function capitalizeStrings(words) {
+    return words.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+  }
+  
+  const words = ["apple", "banaNA", "kiWi", "ORANGE"];
+  console.log(capitalizeStrings(words)); // ["Apple", "Banana", "Kiwi", "Orange"]
+  
